eacd8c2eff8f12e74a6f5eddec40357f3b3e9316
samir sabiee <samirsabiee@gmail.com>
2021-07-18 03:11:12 +0430

    complete add product

--- pages/addProduct.vue
+++ pages/addProduct.vue
@@ -1,63 +1,92 @@
 <template>
   <div class="p-5">
-    <b-form>
+    <b-form id="addForm" ref="form" @submit.stop.prevent="addProduct">
       <b-form-group
         id="input-group-1"
-        label="Your Name:"
+        label="عنوان محصول:"
         label-for="input-1"
       >
         <b-form-input
           id="input-1"
           type="text"
-          v-model="product.name"
-          placeholder="Enter name"
+          v-model="product.title"
+          placeholder="عنوان محصول"
           required
-        ></b-form-input>
+        />
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Text:" label-for="input-2">
+      <b-form-group id="input-group-2" label="توضیحات محصول" label-for="input-2">
         <b-form-input
           id="input-2"
-          placeholder="Enter text"
-          v-model="product.text"
+          placeholder="توضیحات محصول"
+          v-model="product.description"
           required
-        ></b-form-input>
+        />
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-3"
+        label="قیمت محصول:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-3"
+          type="number"
+          v-model="product.price"
+          placeholder="قیمت محصول"
+          required
+        />
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-4"
+        label="لینک تصویر محصول:"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-4"
+          type="text"
+          v-model="product.image"
+          placeholder="لینک تصویر محصول"
+          required
+        />
       </b-form-group>
 
       <!--<b-form-group id="input-group-3" label="انتشار" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          required
-          v-model="product.select"
-        >
-          <b-form-select-option value="1">1</b-form-select-option>
-          <b-form-select-option value="2">2</b-form-select-option>
-          <b-form-select-option value="3">3</b-form-select-option>
-        </b-form-select>
-      </b-form-group>-->
+       <b-form-select
+         id="input-3"
+         required
+         v-model="product.select"
+       >
+         <b-form-select-option value="1">1</b-form-select-option>
+         <b-form-select-option value="2">2</b-form-select-option>
+         <b-form-select-option value="3">3</b-form-select-option>
+       </b-form-select>
+     </b-form-group>-->
 
-      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          id="checkboxes-4"
-          v-model="product.select"
-          :aria-describedby="ariaDescribedby"
-        >
-          <b-form-checkbox v-for="(select,index) in selectOption" :key="index" :value="select.value">{{select.label}}
-          </b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
+      <!--      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">-->
+      <!--        <b-form-checkbox-group-->
+      <!--          id="checkboxes-4"-->
+      <!--          v-model="product.select"-->
+      <!--          :aria-describedby="ariaDescribedby"-->
+      <!--        >-->
+      <!--          <b-form-checkbox v-for="(select,index) in selectOption" :key="index" :value="select.value">{{select.label}}-->
+      <!--          </b-form-checkbox>-->
+      <!--        </b-form-checkbox-group>-->
+      <!--      </b-form-group>-->
 
-      <b-form-group label="نوع" v-slot="{ ariaDescribedby }">
-        <b-form-radio v-for="(radio,index) in radioOption" :key="index" v-model="product.radio"
-                      :aria-describedby="ariaDescribedby" :value="radio.value">{{radio.label}}
+      <b-form-radio-group required label="دسته بندی" v-slot="{ ariaDescribedby }">
+        <b-form-radio v-for="(category, index) in categories" :key="index" v-model="product.category"
+                      :aria-describedby="ariaDescribedby" :value="category">
+          {{ category }}
         </b-form-radio>
-      </b-form-group>
+      </b-form-radio-group>
 
-      <b-form-group label="تاریخ">
-        <date-picker v-model="product.date" :modal="true" from="1400" to="1500" :column="1" mode="single"></date-picker>
-      </b-form-group>
+      <!--      <b-form-group label="تاریخ">-->
+      <!--        <date-picker :modal="true" from="1400" to="1500" :column="1" mode="single"></date-picker>-->
+      <!--      </b-form-group>-->
 
-      <b-button @click="addProduct" variant="primary">ذخیره</b-button>
+      <b-button type="submit" variant="primary">ذخیره</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
 
     </b-form>
@@ -65,39 +94,42 @@
 </template>
 
 <script>
+  import {mapGetters} from "vuex";
+
   export default {
     name: "addProduct",
     data() {
       return {
-        selectOption: [],
-        radioOption: [
-          {label: 'تست یک', value: 'test1'},
-          {label: 'تست دو', value: 'test2'}
-        ],
         product: {
-          name: '',
-          text: '',
-          select: [],
-          radio: '',
-          date: ''
+          title: null,
+          price: null,
+          description: null,
+          image: null,
+          category: null
         }
       }
     },
+
     mounted() {
-      this.getOptions()
+      if (!this.categories.length)
+        this.$store.dispatch('product/fetchCategories')
+      if (!this.products.length)
+        this.$store.dispatch('product/fetch')
     },
+
     methods: {
-      async addProduct() {
-        const newProduct = await this.$axios.post('product', {
-          ...this.product,
-          select: this.product.select.join(' ')
-        })
-        console.log(newProduct)
-      },
-      getOptions() {
-        this.$axios.$get('get/select/options').then(({data}) => {
-          this.selectOption = data
-        })
+      addProduct() {
+        this.$store.dispatch('product/store', this.product)
+      }
+    },
+
+    computed: {
+      ...mapGetters({
+        products: 'product/all',
+        categories: 'product/categories'
+      }),
+      titleValidation() {
+        return this.product.title !== null
       }
     },
   }
